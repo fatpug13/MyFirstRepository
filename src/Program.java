b2fff4698b1e1e9768619f7e49cf121870addde5
Alex R <badfatpug13@gmail.com>
2020-09-04 16:24:41 +0400

    substring occurrence

---
+++ src/Program.java
@@ -0,0 +1,31 @@
+
+public class Program {
+
+	public static void main(String[] args) {	
+		var a = "Targaryen";
+		var b = "Ts";
+		//var c = 35;
+		
+		System.out.println(startsWith(a, b));
+			
+	}
+	
+	public static boolean startsWith(String text, String substr) {
+		
+		var lengthtSubstr = substr.length() -1;
+		var resText = "";
+		var resSubstr = "";
+		 
+		for (var i = 0; i <= lengthtSubstr; i ++) {
+			 
+			resText += text.charAt(i);
+			resSubstr += substr.charAt(i);
+			 
+			 
+			}
+		return resText.equals(resSubstr);
+		}
+		
+		 
+
+}
