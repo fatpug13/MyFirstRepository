dadd1aa8c8fe8e58b317f79585cc80051ed7c8f3
Alex R <badfatpug13@gmail.com>
2020-09-08 17:56:45 +0400

    onlyOdometer

--- src/Level1.java
+++ src/Level1.java
@@ -1,23 +1,6 @@
 
 public class Level1 {
 
-	public static int squirrel(int N)
-
-	{
-		var result = 1;
-
-		if (N == 0 || N == 1) {
-			return result;
-		} else {
-			for (var i = 1; i <= N; i++) {
-				result = result * i;
-			}
-
-			var firstInteger = Integer.parseInt(Integer.toString(result).substring(0, 1));
-			return firstInteger;
-		}
-	}
-
 	public static int odometer(int[] oksana) {
 		var distance = 0;
 		var previousTime = 0;
