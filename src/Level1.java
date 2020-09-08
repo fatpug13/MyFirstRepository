b3a2b8392a3844d063e0f5aad60ba44aca9a82e6
Alex R <badfatpug13@gmail.com>
2020-09-08 17:36:42 +0400

    odometr

--- src/Level1.java
+++ src/Level1.java
@@ -1,22 +1,42 @@
-import java.util.*;
 
-public class Level1 
-{
-	public static int squirrel (int N) 
+public class Level1 {
+
+	public static int squirrel(int N)
+
 	{
 		var result = 1;
-		
+
 		if (N == 0 || N == 1) {
 			return result;
-		} 
-		else {	
-		for (var i = 1; i <= N; i++)
-		{
-			result = result * i; 
+		} else {
+			for (var i = 1; i <= N; i++) {
+				result = result * i;
+			}
+
+			var firstInteger = Integer.parseInt(Integer.toString(result).substring(0, 1));
+			return firstInteger;
 		}
-		  
-		var firstInteger = Integer.parseInt(Integer.toString(result).substring(0,1));	
-		return firstInteger;
+	}
+
+	public static int odometer(int[] oksana) {
+		var distance = 0;
+		var previousTime = 0;
+
+		for (var i = 0; i <= oksana.length - 1;) {
+			var v = oksana[i];
+			var currentTime = oksana[i + 1];
+
+			if (i == 0) {
+				previousTime = 0;
+			} else {
+				previousTime = oksana[i - 1];
+			}
+
+			distance = v * (currentTime - previousTime) + distance;
+
+			i = i + 2;
+
 		}
+		return distance;
 	}
 }
--- src/Program.java
+++ src/Program.java
@@ -1,45 +1,65 @@
 
 public class Program {
 
-	public static void main(String[] args) {	
-		var a = 5;
-		//var b = "Ts";
+	public static void main(String[] args) {
+		// var a = 5;
+		int arr[] = new int[] { 10, 1, 20, 2};
 
-		System.out.println(squirrel(a));
+		// System.out.println(squirrel(a));
+		System.out.println(odometer(arr));
 	}
-	
-	public static boolean startsWith(String text, String substr) 
+
+	public static boolean startsWith(String text, String substr)
 
 	{
-		
-		var lengthtSubstr = substr.length() -1;
+
+		var lengthtSubstr = substr.length() - 1;
 		var resText = "";
 		var resSubstr = "";
-		 
-		for (var i = 0; i <= lengthtSubstr; i ++) {
-			 
+
+		for (var i = 0; i <= lengthtSubstr; i++) {
+
 			resText += text.charAt(i);
 			resSubstr += substr.charAt(i);
-			 		 
-			}
+
+		}
 		return resText.equals(resSubstr);
 	}
-	
-	public static int squirrel (int N) 
-	{
+
+	public static int squirrel(int N) {
 		var result = 1;
-		
+
 		if (N == 0 || N == 1) {
 			return result;
-		} 
-		else {	
-		for (var i = 1; i <= N; i++)
-		{
-			result *= i;  
+		} else {
+			for (var i = 1; i <= N; i++) {
+				result *= i;
+			}
+
+			var firstInteger = Integer.parseInt(Integer.toString(result).substring(0, 1));
+			return firstInteger;
 		}
-		
-		var firstInteger = Integer.parseInt(Integer.toString(result).substring(0,1));
-		return firstInteger;
+	}
+
+	public static int odometer(int[] oksana) {
+		var distance = 0;
+		var previousTime = 0;
+
+		for (var i = 0; i <= oksana.length - 1;) {
+			var v = oksana[i];
+			var currentTime = oksana[i + 1];
+
+			if (i == 0) {
+				previousTime = 0;
+			} else {
+				previousTime = oksana[i - 1];
+			}
+
+			distance = v * (currentTime - previousTime) + distance;
+
+			i = i + 2;
+
 		}
+		return distance;
 	}
 }
