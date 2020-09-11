76d137a24ca855594983bdc305e1f75c0657c35e
Alex R <badfatpug13@gmail.com>
2020-09-11 11:09:49 +0400

    ConquestCampaign

--- src/Level1.java
+++ src/Level1.java
@@ -1,25 +1,84 @@
 
 public class Level1 {
 
-	public static int odometer(int[] oksana) {
-		var distance = 0;
-		var previousTime = 0;
+	public static int ConquestCampaign(int N, int M, int L, int[] battalion) {
 
-		for (var i = 0; i <= oksana.length - 1;) {
-			var v = oksana[i];
-			var currentTime = oksana[i + 1];
+		int[][] battleground = new int[N][M];
 
-			if (i == 0) {
-				previousTime = 0;
-			} else {
-				previousTime = oksana[i - 1];
+		for (var g = 0; g <= battalion.length - 1;) {
+
+			var coordinateN = battalion[g];
+			var coordinateM = battalion[g + 1];
+
+			battleground[coordinateN - 1][coordinateM - 1] = 1;
+			g = g + 2;
+		}
+
+		var currentDay = 2;
+		var val = 0;
+
+		do {
+
+			for (var i = 0; i < battleground.length; i++) {
+
+				for (var j = 0; j < battleground[i].length; j++) {
+
+					if (battleground[i][j] != 0) {
+
+						if (battleground[i][j] < currentDay) {
+
+							if (j - 1 >= 0) {
+
+								if (battleground[i][j - 1] == 0) {
+									battleground[i][j - 1] = currentDay;
+								}
+							}
+
+							if (j + 1 <= battleground[i].length - 1) {
+
+								if (battleground[i][j + 1] == 0) {
+									battleground[i][j + 1] = currentDay;
+								}
+							}
+
+							if (i + 1 <= battleground.length - 1) {
+
+								if (battleground[i + 1][j] == 0) {
+									battleground[i + 1][j] = currentDay;
+								}
+							}
+
+							if (i - 1 >= 0) {
+
+								if (battleground[i - 1][j] == 0) {
+									battleground[i - 1][j] = currentDay;
+								}
+							}
+						}
+					}
+
+				}
+
 			}
 
-			distance = v * (currentTime - previousTime) + distance;
+			for (var i = 0; i < battleground.length; i++) {
+				for (var j = 0; j < battleground[i].length; j++) {
 
-			i = i + 2;
+					if (battleground[i][j] == 0) {
+						val = val - 1;
+						break;
+					} else {
+						val = 1;
+					}
 
-		}
-		return distance;
+				}
+			}
+
+			currentDay = currentDay + 1;
+
+		} while (val <= 0);
+
+		return currentDay - 1;
 	}
+
 }
--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,19 @@
+import java.util.*;
+import java.util.Arrays;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		// var a = 5;
-		int arr[] = new int[] { 10, 1, 20, 2};
+		int arr[] = new int[] { 2, 2, 3, 4,};
+		var N = 3;
+		var M = 4;
+		var L = 2;
 
 		// System.out.println(squirrel(a));
-		System.out.println(odometer(arr));
+		// System.out.println(odometer(arr));
+		System.out.println(ConquestCampaign(N, M, L, arr));
 	}
 
 	public static boolean startsWith(String text, String substr)
@@ -61,5 +68,103 @@ public class Program {
 
 		}
 		return distance;
+	}
+
+	public static  int ConquestCampaign(int N, int M, int L, int[] battalion) {
+		
+		// создаем двумерный массив по пришедшим размерам
+		int[][] battleground = new int[N][M];
+
+		// ¬ысадка по начальным координатам. 1й день
+		for (var g = 0; g <= battalion.length - 1;) {
+
+			var coordinateN = battalion[g];
+			var coordinateM = battalion[g + 1];
+
+			battleground[coordinateN - 1][coordinateM - 1] = 1;
+			g = g + 2;
+
+		}
+
+		// текущий день и счетчик дней
+		var currentDay = 2;
+		var val = 0;
+
+		// Ќачинаем захват со второго дн€
+		do {
+			// обойдем массив в цикле. ÷икл по N координатам (строкам)
+			for (var i = 0; i < battleground.length; i++) {
+
+				// цикл по M координатам (столбцамы)
+				for (var j = 0; j < battleground[i].length; j++) {
+
+					// Ќайдем заполненные элменты массива в первый день высадки
+					if (battleground[i][j] != 0) {
+
+						// «ахват территори происходит в 4 направлени€х (крестом). ≈сли элемент содержит
+						// не ноль и текущий день,
+						// то захватывать не надо.
+						if (battleground[i][j] < currentDay) {
+
+							// сдвиг по строке влево
+							// »ндекс в границе массива?
+							if (j - 1 >= 0) {
+								// значение заполнено нулем?
+								if (battleground[i][j - 1] == 0) {
+									battleground[i][j - 1] = currentDay;
+								}
+							}
+							// сдвиг по строке вправо
+							// »ндекс в границе массива?
+							if (j + 1 <= battleground[i].length - 1) {
+								// значение заполнено нулем
+								if (battleground[i][j + 1] == 0) {
+									battleground[i][j + 1] = currentDay;
+								}
+							}
+							// сдвиг вниз
+							// »ндекс в границе массива?
+							if (i + 1 <= battleground.length - 1) {
+								// значение заполнено нулем?
+								if (battleground[i + 1][j] == 0) {
+									battleground[i + 1][j] = currentDay;
+								}
+							}
+							// сдвиг вверх
+							// »ндекс в границе массива?
+							if (i - 1 >= 0) {
+								// значение заполнено нулем?
+								if (battleground[i - 1][j] == 0) {
+									battleground[i - 1][j] = currentDay;
+								}
+							}
+						}
+					}
+
+				}
+
+			}
+
+			// ≈сли в массиве есть ноль, значит нам нужен еще один день дл€ захвата
+			for (var i = 0; i < battleground.length; i++) {
+				for (var j = 0; j < battleground[i].length; j++) {
+
+					if (battleground[i][j] == 0) {
+						val = val - 1;
+						break;
+					} else {
+						val = 1;
+					}
+
+				}
+			}
+
+			currentDay = currentDay + 1;
+
+		} while (val <= 0);
+		System.out.println(Arrays.deepToString(battleground));
+
+		// минус 1 день т.к. цикл началс€ со 2го (в 1й день была высыдка)
+		return currentDay - 1;
 	}
 }
