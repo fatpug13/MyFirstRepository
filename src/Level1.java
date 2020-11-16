8543d1f3e84105eb7214773f441d23eea00f3017
Alex R <badfatpug13@gmail.com>
2020-11-16 11:49:53 +0400

    maximumDiscount

--- src/Level1.java
+++ src/Level1.java
@@ -3,94 +3,45 @@ import java.util.*;
 
 public class Level1 {
 
-	public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
+	public static int MaximumDiscount(int N, int[] price) {
 
-		boolean result = true;
+		int result = 0;
+		Arrays.sort(price);
 
-		String newS1 = "";
-		newS1 = deleteSpace(S1, newS1);
-		String newS2 = "";
-		newS2 = deleteSpace(S2, newS2);
+		if (N == 3) {
+			result = price[0];
+		} else if (N > 3) {
 
-		String[][] map = new String[H1][W1];
-		fillArray(map, newS1);
-		String[][] groupingOfTanks = new String[H2][W2];
-		fillArray(groupingOfTanks, newS2);
+			int countFreeProd = N / 3;
+			int discount = 0;
 
-		ArrayList<Integer> res = new ArrayList<>();
+			for (int i = 0; i < countFreeProd; i++) {
+				discount += price[i];
+			}
 
-		boolean marker = false;
-		int a = 0;
-		int b = 0;
-		String strMap = "";
-		String strGrTanks = "";
+			int anotherDiscount = 0;
 
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
+			for (int i = N - 3; i >= 0;) {
 
-				for (int k = 0; k < groupingOfTanks.length; k++) {
-					for (int l = 0; l < groupingOfTanks[k].length; l++) {
-
-						strMap = map[i][j];
-						strGrTanks = groupingOfTanks[k][l];
-						a = Integer.parseInt(strMap);
-						b = Integer.parseInt(strGrTanks);
-
-						if (a == b) {
-							marker = true;
-						}
-
-						strMap = "";
-						strGrTanks = "";
-						a = 0;
-						b = 0;
-
-					}
+				if (countFreeProd != 0) {
+					anotherDiscount += price[i];
 				}
 
+				countFreeProd--;
+				i -= 3;
 			}
 
-			if (marker == true) {
-				res.add(1);
+			if (discount > anotherDiscount) {
+				result = discount;
 			} else {
-				res.add(0);
+				result = anotherDiscount;
 			}
-		}
 
-		for (int res1 : res) {
-			if (res1 != 0) {
-				result = true;
-			} else {
-				result = false;
-				break;
-			}
+		} else {
+
+			result = 0;
 		}
 
 		return result;
-	}
-
-	public static void fillArray(String[][] arr, String str) {
-
-		String substr = "";
-		int counterStr = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-
-				substr += str.charAt(counterStr);
-				arr[i][j] = substr;
-				substr = "";
-				counterStr += 1;
-			}
-		}
-	}
-
-	public static String deleteSpace(String str, String newStr) {
-
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) != ' ') {
-				newStr += str.charAt(i);
-			}
-		}
-		return newStr;
 	}
 }
